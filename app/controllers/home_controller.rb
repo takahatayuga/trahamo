2456c745712df53c00ce9f3d877c19649382b1d6
高畑 <takahata-yuga@takabatanoMacBook-Air.local>
2020-06-09 23:37:07 +0900

    キャッシュを削除

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  def top
-  end
-
-  def search
-    redirect_to("https://www.nicovideo.jp/search/tryangle_harmony%20#{params[:number]}回?ref=nicotop_search")
-  end
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  get "/" => "home#top"
-  get "home/top" => "home#top"
-  post "search" => "home#search"
-end
