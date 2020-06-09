fefff09cbe81f927afd7388d834f5ffcf2572832
高畑 <takahata-yuga@takabatanoMacBook-Air.local>
2020-06-09 22:52:17 +0900

    検索ページにリダイレクトするように変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def search
+    redirect_to("https://www.nicovideo.jp/search/tryangle_harmony%20#{params[:number]}回?ref=nicotop_search")
+  end
 end
